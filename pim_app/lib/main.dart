53f442caa798290fc1390780b445bf77f74e6c3d
CristiArde <c_burns93@yahoo.ca>
2021-06-07 09:33:55 +0200

    added base model for scoped model pattern

--- pim_app/lib/main.dart
+++ pim_app/lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import "utils.dart" as utils;
 
 void main() {
-  runApp(MyApp());
+  startMeUp() async {
+    Directory docsDir = await getApplicationDocumentsDirectory();
+    utils.docsDir = docsDir;
+    runApp(MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,20 +17,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Bobobo"),
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.date_range), text: "Appointments"),
+                  Tab(icon: Icon(Icons.contacts), text: "Contacts"),
+                  Tab(icon: Icon(Icons.note), text: "Notes"),
+                  Tab(icon: Icon(Icons.assignment_turned_in), text: "Tasks")
+                ],
+              ),
+            ),
+            body: TabBarView(
+                // children: [Appointments(), Contacts(), Notes(), Tasks()])),
+                children: [])),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
